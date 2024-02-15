d693ae903a029235e71072cf1ceab6cfe0e441ab
savaz-20 <160168903+savaz-20@users.noreply.github.com>
2024-02-15 23:48:44 +0400

    Upload Files

---
+++ Steam Checker/utils/JsonAlysisUtil.java
@@ -0,0 +1,20 @@
+package mirai.noerla.steam_checker.utils;
+
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Optional;
+
+import static mirai.noerla.steam_checker.consts.GloalConsts.PRICE_NOT_FOUND;
+
+public class JsonAlysisUtil {
+
+    public static String getNowPrice(JSONObject jsonObject, String gameId){
+        return Optional.ofNullable(jsonObject)
+                .map(j -> j.getJSONObject(gameId))
+                .map(j -> j.getJSONObject("data"))
+                .map(j -> j.getJSONObject("price_overview"))
+                .map(j -> (String)j.get("final_formatted"))
+                .orElse(PRICE_NOT_FOUND);
+    }
+}
