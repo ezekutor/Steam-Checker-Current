38aab6bb15a361f88d6f2d6dc0be19b35969b911
savaz-20 <160168903+savaz-20@users.noreply.github.com>
2024-02-15 23:47:24 +0400

    Upload Files

---
+++ Steam Checker/consts/CountryConsts.java
@@ -0,0 +1,13 @@
+package mirai.noerla.steam_checker.consts;
+
+/**
+ * 国家代码
+ * todo 后期考虑从配置文件读取
+ */
+public class CountryConsts {
+    public static final String CN = "CN";
+    public static final String HK = "HK";//香港
+    public static final String AR = "AR";//阿根廷
+    public static final String RU = "RU";//俄罗斯
+    public static final String TRY = "TR";//土耳其
+}
